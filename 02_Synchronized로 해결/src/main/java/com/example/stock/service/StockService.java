5b61aa7e23f8c4ce549f1f9f0b705d71b8d0360a
lcomment <kohyunsuk98@gmail.com>
2022-11-22 18:19:24 +0900

    docs: 주석 추가

--- 02_Synchronized로 해결/src/main/java/com/example/stock/service/StockService.java
+++ 02_Synchronized로 해결/src/main/java/com/example/stock/service/StockService.java
@@ -12,7 +12,14 @@ public class StockService {
     public StockService(StockRepository stockRepository){
         this.stockRepository = stockRepository;
     }
-    
+
+    /*
+     * synchronized를 메서드 선언부에 붙여줌
+     * → 해당 메서드에 한 개의 thread만 접근 가능
+     * But. Test 실패
+     * @Transactional → 해당 메서드를 갖는 클래스를 새로 만들어서 실행
+     * 따라서 @Transactional 을 주석처리 해야 함
+     */
     @Transactional
     public synchronized void decrease(Long id, Long quantity){
         Stock stock = stockRepository.findById(id).orElseThrow();
