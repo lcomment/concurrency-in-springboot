2d20aab182cfeeee0f9f74b568e0e2cd6b3d409d
lcomment <kohyunsuk98@gmail.com>
2022-11-22 18:19:06 +0900

    feat: synchronized 추가

--- 02_Synchronized로 해결/src/main/java/com/example/stock/service/StockService.java
+++ 02_Synchronized로 해결/src/main/java/com/example/stock/service/StockService.java
@@ -3,6 +3,7 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -11,8 +12,9 @@ public class StockService {
     public StockService(StockRepository stockRepository){
         this.stockRepository = stockRepository;
     }
-
-    public void decrease(Long id, Long quantity){
+    
+    @Transactional
+    public synchronized void decrease(Long id, Long quantity){
         Stock stock = stockRepository.findById(id).orElseThrow();
 
         stock.decrease(quantity);
