201c644d28bb8ffcc160e705653e58d052edc546
lcomment <kohyunsuk98@gmail.com>
2023-02-05 14:47:47 +0900

    feature: redisson으로 동시성 처리

---
+++ 04_Redis로 처리/Redisson_Practice/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# concurrency-in-springboot
+# 스프링부트에서의 동시성 처리
+
+## 1. synchronized
+
+- 해당 메서드에 한 개의 thread만 접근 가능하게 해줌
+- 서버가 2개 이상일 때는 Race Condition이 발생할 수 있음
+
+## 2. DB의 Lock 사용하기
